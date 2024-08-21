bf5a73dca058741a4b371fecb0ae5a92995f8392
kmmaniez <dzlabsid@gmail.com>
2024-08-21 11:27:19 +0700

    update terbilang & tambah kasir

--- resources/views/pages/kasir/invoice-dikirim.blade.php
+++ resources/views/pages/kasir/invoice-dikirim.blade.php
@@ -1,3 +1,41 @@
+@php
+function penyebut($nilai) {
+		$nilai = abs($nilai);
+		$huruf = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
+		$temp = "";
+		if ($nilai < 12) {
+			$temp = " ". $huruf[$nilai];
+		} else if ($nilai <20) {
+			$temp = penyebut($nilai - 10). " belas";
+		} else if ($nilai < 100) {
+			$temp = penyebut($nilai/10)." puluh". penyebut($nilai % 10);
+		} else if ($nilai < 200) {
+			$temp = " seratus" . penyebut($nilai - 100);
+		} else if ($nilai < 1000) {
+			$temp = penyebut($nilai/100) . " ratus" . penyebut($nilai % 100);
+		} else if ($nilai < 2000) {
+			$temp = " seribu" . penyebut($nilai - 1000);
+		} else if ($nilai < 1000000) {
+			$temp = penyebut($nilai/1000) . " ribu" . penyebut($nilai % 1000);
+		} else if ($nilai < 1000000000) {
+			$temp = penyebut($nilai/1000000) . " juta" . penyebut($nilai % 1000000);
+		} else if ($nilai < 1000000000000) {
+			$temp = penyebut($nilai/1000000000) . " milyar" . penyebut(fmod($nilai,1000000000));
+		} else if ($nilai < 1000000000000000) {
+			$temp = penyebut($nilai/1000000000000) . " trilyun" . penyebut(fmod($nilai,1000000000000));
+		}     
+		return $temp;
+	}
+ 
+	function terbilang($nilai) {
+		if($nilai<0) {
+			$hasil = "minus ". trim(penyebut($nilai));
+		} else {
+			$hasil = trim(penyebut($nilai));
+		}     		
+		return $hasil;
+	}
+@endphp
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,6 +52,7 @@
     <br>
     <div style="display: flex; justify-content: space-between">
         <div style="display: flex; flex-direction: column; gap: 8px">
+            <span>Kasir : {{ $user['name'] }}</span>
             <span>Kepada Yth:</span>
             <span>{{ $data[0]['nama_pembeli'] }}</span>
             <span>{{ $data[0]['alamat_pembeli'] }}</span>
@@ -57,6 +96,9 @@
         </tr>
         </tbody>
     </table>
+    <div>
+        <p>Terbilang : <i><strong>{{ Str::upper(terbilang($subtotal)) }}</strong></i></p>
+    </div>
     <div style="display: flex; justify-content: space-between; margin-top: 2rem">
         <div style="display: flex; flex-direction: column; gap: 3.5rem; align-items: center">
             <span>Diterima oleh,</span>
