cff80a291c7d436a6f9d6a1b282738715dd56105
kmmaniez <dzlabsid@gmail.com>
2024-08-22 10:25:31 +0700

    final rekap penjualan

--- app/Http/Controllers/Admin/RekapPenjualanController.php
+++ app/Http/Controllers/Admin/RekapPenjualanController.php
@@ -27,35 +27,52 @@ class RekapPenjualanController extends Controller
                 $startdate = $request->start_date;
                 $enddate = $request->end_date;
 
-                $data =   PenjualanBarang::select(
+                $data = PenjualanBarang::all([
                     'nama_barang',
-                    DB::raw('SUM(quantity) as item_terjual'),
+                    'tgl_pembelian',
                     'harga_barang',
-                    DB::raw('SUM(quantity * harga_barang) as total_penjualan')
-                )
-                    ->whereBetween('tgl_pembelian', [$request->start_date, $request->end_date])
-                    // ->whereMonth('tgl_pembelian', date('m'))->whereYear('tgl_pembelian', date('Y'))
-                    ->groupBy('nama_barang', 'harga_barang');
-            } else {
+                    'quantity',
+                ])->whereBetween('tgl_pembelian', [$request->start_date, $request->end_date]);
 
-                $data =   PenjualanBarang::select(
+                // $data =   PenjualanBarang::select(
+                //     'nama_barang',
+                //     DB::raw('SUM(quantity) as item_terjual'),
+                //     'harga_barang',
+                //     DB::raw('SUM(quantity * harga_barang) as total_penjualan')
+                // )
+                //     ->whereBetween('tgl_pembelian', [$request->start_date, $request->end_date])
+                //     // ->whereMonth('tgl_pembelian', date('m'))->whereYear('tgl_pembelian', date('Y'))
+                //     ->groupBy('nama_barang', 'harga_barang');
+
+            } else {
+                $data = PenjualanBarang::all([
                     'nama_barang',
-                    DB::raw('SUM(quantity) as item_terjual'),
+                    'tgl_pembelian',
                     'harga_barang',
-                    DB::raw('SUM(quantity * harga_barang) as total_penjualan')
-                )->whereMonth('tgl_pembelian', date('m'))->whereYear('tgl_pembelian', date('Y'))
-                    ->groupBy('nama_barang', 'harga_barang');
+                    'quantity',
+                ]);
+                // $data =   PenjualanBarang::select(
+                //     'nama_barang',
+                //     DB::raw('SUM(quantity) as item_terjual'),
+                //     'harga_barang',
+                //     DB::raw('SUM(quantity * harga_barang) as total_penjualan')
+                // )->whereMonth('tgl_pembelian', date('m'))->whereYear('tgl_pembelian', date('Y'))
+                //     ->groupBy('nama_barang', 'harga_barang');
             }
             // SELECT nama_barang, SUM(quantity) as item_terjual, harga_barang, tgl_pembelian, SUM(quantity*harga_barang) as total_penjualan FROM `penjualan_barangs` WHERE MONTH(tgl_pembelian) = 8 AND YEAR(tgl_pembelian) = 2024 GROUP BY nama_barang,harga_barang,tgl_pembelian;
             $total_harga_item = 0;
             $total_harga_penjualan = 0;
-            foreach ($data->get() as $key => $value) {
+            foreach ($data as $key => $value) {
+                // $total_harga_penjualan = $value['harga_barang'] * $value['quantity'];
                 $total_harga_item += $value['harga_barang'];
-                $total_harga_penjualan += $value['total_penjualan'];
+                $total_harga_penjualan += $value['harga_barang'] * $value['quantity'];
             }
 
             return DataTables::of($data)
                 ->addIndexColumn()
+                ->addColumn('total_penjualan', function ($object) {
+                    return $object->harga_barang * $object->quantity;
+                })
                 ->addColumn('total_harga_item', function ($object) use ($total_harga_item) {
                     return $total_harga_item;
                 })
