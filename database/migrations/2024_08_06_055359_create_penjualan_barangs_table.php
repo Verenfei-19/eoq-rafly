98bc7aaadd151619721b0c1a5fcb27a05a452155
kmmaniez <dzlabsid@gmail.com>
2024-09-09 18:34:57 +0700

    add kolom barang id

--- database/migrations/2024_08_06_055359_create_penjualan_barangs_table.php
+++ database/migrations/2024_08_06_055359_create_penjualan_barangs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('penjualan_barangs', function (Blueprint $table) {
             $table->id();
             // $table->foreign('id_barang')->references('barang_id')->on('barangs');
+            $table->string('id_barang');
             $table->string('nama_barang');
             $table->string('invoice_number');
             $table->string('nama_pembeli');
