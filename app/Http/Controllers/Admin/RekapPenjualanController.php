4c26bbc6a4101965cc4983aae3600700fd0b4f0d
kmmaniez <dzlabsid@gmail.com>
2024-09-24 08:22:29 +0700

    update rekap penjualan controller

--- app/Http/Controllers/Admin/RekapPenjualanController.php
+++ app/Http/Controllers/Admin/RekapPenjualanController.php
@@ -28,33 +28,48 @@ class RekapPenjualanController extends Controller
                 $startdate = $request->start_date;
                 $enddate = $request->end_date;
 
-                $data = PenjualanBarang::all([
-                    'id_barang',
-                    'nama_barang',
-                    'tgl_pembelian',
-                    'harga_barang',
-                    'quantity',
-                ])->whereBetween('tgl_pembelian', [$startdate, $request->end_date]);
+                // $data = PenjualanBarang::all([
+                //     'id_barang',
+                //     'nama_barang',
+                //     'tgl_pembelian',
+                //     'harga_barang',
+                //     'quantity',
+                // ])->whereBetween('tgl_pembelian', [$startdate, $request->end_date]);
+                $query = "
+                SELECT id_barang, nama_barang, SUM(quantity) AS total_quantity, SUM(quantity * harga_barang) AS total_harga 
+                FROM penjualan_barang_details WHERE tgl_pembelian BETWEEN '$startdate' AND '$enddate' GROUP BY id_barang, nama_barang;
+                ";
+                $data = DB::select($query);
             } else if ($request->filled('start_date')) {
-                $data = PenjualanBarang::all([
-                    'id_barang',
-                    'nama_barang',
-                    'tgl_pembelian',
-                    'harga_barang',
-                    'quantity',
-                ])->where('tgl_pembelian', "$request->start_date");
+                // $data = PenjualanBarang::all([
+                //     'id_barang',
+                //     'nama_barang',
+                //     'tgl_pembelian',
+                //     'harga_barang',
+                //     'quantity',
+                // ])->where('tgl_pembelian', "$request->start_date");
+                $query = "
+                SELECT id_barang, nama_barang, SUM(quantity) AS total_quantity, SUM(quantity * harga_barang) AS total_harga 
+                FROM penjualan_barang_details WHERE tgl_pembelian = '$request->start_date' GROUP BY id_barang, nama_barang;
+                ";
+                $data = DB::select($query);
             } else {
                 // $query = '
                 // SELECT nama_barang, SUM(quantity) as quantity, tgl_pembelian,harga_barang, SUM(quantity*harga_barang) as total FROM `penjualan_barangs` 
                 // WHERE tgl_pembelian = "' . date('Y-m-d') . '" GROUP BY nama_barang,harga_barang
                 // ';
-                $data = PenjualanBarang::all([
-                    'id_barang',
-                    'nama_barang',
-                    'tgl_pembelian',
-                    'harga_barang',
-                    'quantity',
-                ]);
+                // $data = PenjualanBarang::all([
+                //     'id_barang',
+                //     'nama_barang',
+                //     'tgl_pembelian',
+                //     'harga_barang',
+                //     'quantity',
+                // ]);
+                $query = "
+                SELECT id_barang, nama_barang, SUM(quantity) AS total_quantity, SUM(quantity * harga_barang) AS total_harga 
+                FROM penjualan_barang_details WHERE tgl_pembelian = '" . date('Y-m-d') . "' GROUP BY id_barang, nama_barang;
+                ";
+                $data = DB::select($query);
                 // $query = '
                 // SELECT nama_barang, SUM(quantity) as quantity, harga_barang, SUM(quantity*harga_barang) as total FROM `penjualan_barangs` GROUP BY nama_barang,harga_barang;
                 // ';
@@ -65,22 +80,22 @@ class RekapPenjualanController extends Controller
             $total_harga_penjualan = 0;
             foreach ($data as $key => $value) {
                 // $total_harga_item += $value['harga_barang'];
-                $total_harga_item += $value->harga_barang;
+                $total_harga_item += $value->total_harga;
                 // $total_harga_penjualan += $value['harga_barang'] * $value['quantity'];
-                $total_harga_penjualan += $value->harga_barang * $value->quantity;
+                $total_harga_penjualan += $value->total_harga * $value->total_quantity;
             }
 
             return DataTables::of($data)
                 ->addIndexColumn()
-                ->addColumn('total_penjualan', function ($object) {
-                    return $object->harga_barang * $object->quantity;
-                })
-                // ->addColumn('total_harga_item', function ($object) use ($total_harga_item) {
-                //     return $total_harga_item;
+                // ->addColumn('total_penjualan', function ($object) {
+                //     return $object->harga_barang * $object->quantity;
                 // })
-                ->addColumn('total_harga_penjualan', function ($object) use ($total_harga_penjualan) {
-                    return $total_harga_penjualan;
+                ->addColumn('total_harga_item', function ($object) use ($total_harga_item) {
+                    return $total_harga_item;
                 })
+                // ->addColumn('total_harga_penjualan', function ($object) use ($total_harga_penjualan) {
+                //     return $total_harga_penjualan;
+                // })
                 ->make(true);
         }
         return view('pages.kasir.rekap-penjualan', compact('user'));
