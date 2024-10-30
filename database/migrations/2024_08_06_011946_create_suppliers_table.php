9b485d4c3b04c015254c65601c06c3f8f7bcd83d
kmmaniez <dzlabsid@gmail.com>
2024-10-30 20:00:35 +0700

    Update table supplier

--- database/migrations/2024_08_06_011946_create_suppliers_table.php
+++ database/migrations/2024_08_06_011946_create_suppliers_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('nama');
             $table->string('telepon');
             $table->string('alamat');
+            $table->string('id_barang');
+            $table->integer('waktu');
             $table->timestamps();
         });
     }
