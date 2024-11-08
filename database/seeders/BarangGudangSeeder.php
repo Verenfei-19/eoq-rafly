2170f3a9c72e2b630cd757a53a8fda8a0ca72d62
kmmaniez <dzlabsid@gmail.com>
2024-11-08 18:44:58 +0700

    update seeder

--- database/seeders/BarangGudangSeeder.php
+++ database/seeders/BarangGudangSeeder.php
@@ -26,13 +26,13 @@ class BarangGudangSeeder extends Seeder
                 $barang_gudang->slug = Str::random(16);
                 $barang_gudang->barang_id = $data->barang_id;
                 $barang_gudang->gudang_id = 'G00001';
-                if ($data->nama_barang == 'Buku Tulis BigBoss Campus 50 Lembar' || $data->nama_barang == 'Buku Tulis Sidu 38 Lembar') {
-                    $barang_gudang->stok_masuk = 80;
-                    $barang_gudang->stok_keluar = 60;
-                } else {
-                    $barang_gudang->stok_masuk = 60;
-                    $barang_gudang->stok_keluar = 45;
-                }
+                // if ($data->nama_barang == 'Buku Tulis BigBoss Campus 50 Lembar' || $data->nama_barang == 'Buku Tulis Sidu 38 Lembar') {
+                //     $barang_gudang->stok_masuk = 80;
+                //     $barang_gudang->stok_keluar = 60;
+                // } else {
+                $barang_gudang->stok_masuk = 2500;
+                // $barang_gudang->stok_keluar = 45;
+                // }
 
                 $barang_gudang->save();
                 DB::commit();
