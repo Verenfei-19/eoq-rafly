6440397dbc0a7d8b919429b0a99752504bbdf521
kmmaniez <dzlabsid@gmail.com>
2024-12-04 20:02:17 +0700

    Update controller rekap penjualan

--- app/Http/Controllers/Admin/RekapPenjualanController.php
+++ app/Http/Controllers/Admin/RekapPenjualanController.php
@@ -28,52 +28,23 @@ class RekapPenjualanController extends Controller
                 $startdate = $request->start_date;
                 $enddate = $request->end_date;
 
-                // $data = PenjualanBarang::all([
-                //     'id_barang',
-                //     'nama_barang',
-                //     'tgl_pembelian',
-                //     'harga_barang',
-                //     'quantity',
-                // ])->whereBetween('tgl_pembelian', [$startdate, $request->end_date]);
                 $query = "
                 SELECT id_barang, nama_barang, SUM(quantity) AS total_quantity, SUM(quantity * harga_barang) AS total_harga 
                 FROM penjualan_barang_details WHERE tgl_pembelian BETWEEN '$startdate' AND '$enddate' GROUP BY id_barang, nama_barang;
                 ";
                 $data = DB::select($query);
             } else if ($request->filled('start_date')) {
-                // $data = PenjualanBarang::all([
-                //     'id_barang',
-                //     'nama_barang',
-                //     'tgl_pembelian',
-                //     'harga_barang',
-                //     'quantity',
-                // ])->where('tgl_pembelian', "$request->start_date");
                 $query = "
                 SELECT id_barang, nama_barang, SUM(quantity) AS total_quantity, SUM(quantity * harga_barang) AS total_harga 
                 FROM penjualan_barang_details WHERE tgl_pembelian = '$request->start_date' GROUP BY id_barang, nama_barang;
                 ";
                 $data = DB::select($query);
             } else {
-                // $query = '
-                // SELECT nama_barang, SUM(quantity) as quantity, tgl_pembelian,harga_barang, SUM(quantity*harga_barang) as total FROM `penjualan_barangs` 
-                // WHERE tgl_pembelian = "' . date('Y-m-d') . '" GROUP BY nama_barang,harga_barang
-                // ';
-                // $data = PenjualanBarang::all([
-                //     'id_barang',
-                //     'nama_barang',
-                //     'tgl_pembelian',
-                //     'harga_barang',
-                //     'quantity',
-                // ]);
                 $query = "
                 SELECT id_barang, nama_barang, SUM(quantity) AS total_quantity, SUM(quantity * harga_barang) AS total_harga 
                 FROM penjualan_barang_details WHERE tgl_pembelian = '" . date('Y-m-d') . "' GROUP BY id_barang, nama_barang;
                 ";
                 $data = DB::select($query);
-                // $query = '
-                // SELECT nama_barang, SUM(quantity) as quantity, harga_barang, SUM(quantity*harga_barang) as total FROM `penjualan_barangs` GROUP BY nama_barang,harga_barang;
-                // ';
-                // $data = DB::select($query);
             }
             // SELECT nama_barang, SUM(quantity) as item_terjual, harga_barang, tgl_pembelian, SUM(quantity*harga_barang) as total_penjualan FROM `penjualan_barangs` WHERE MONTH(tgl_pembelian) = 8 AND YEAR(tgl_pembelian) = 2024 GROUP BY nama_barang,harga_barang,tgl_pembelian;
             $total_harga_item = 0;
@@ -87,15 +58,9 @@ class RekapPenjualanController extends Controller
 
             return DataTables::of($data)
                 ->addIndexColumn()
-                // ->addColumn('total_penjualan', function ($object) {
-                //     return $object->harga_barang * $object->quantity;
-                // })
                 ->addColumn('total_harga_item', function ($object) use ($total_harga_item) {
                     return $total_harga_item;
                 })
-                // ->addColumn('total_harga_penjualan', function ($object) use ($total_harga_penjualan) {
-                //     return $total_harga_penjualan;
-                // })
                 ->make(true);
         }
         return view('pages.kasir.rekap-penjualan', compact('user'));
